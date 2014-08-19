1da2a9b12fc0faa237974072ac26a0e7358dbc28
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-19 11:54:44 +0100

    Running art/3d.py

--- art/3d.py
+++ art/3d.py
@@ -1,9 +1,10 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-import math
+import (math,sqrt)
 
 
 def linethree(x1,y1,z1,x2,y2,z2):
+  a1=100+x1*sqrt(3)/2
   line(10,20,30,40)
 
 linethree(1,1,1,1,1,1)
