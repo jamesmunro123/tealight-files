f986b0e1955b8e96e3e3821e82a9130c0b778cc9
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-19 11:52:48 +0100

    Running art/3d.py

--- art/3d.py
+++ art/3d.py
@@ -3,5 +3,5 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 import math
 
 
-def linethree(x1,y1,z1,x2,y2,z2)
+def linethree(x1,y1,z1,x2,y2,z2):
   line(10,20,30,40)
