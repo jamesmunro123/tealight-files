ba1b03a02e8e63933c96aa2705c18b750bef2c4d
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-19 12:02:00 +0100

    Running art/3d.py

--- art/3d.py
+++ art/3d.py
@@ -6,9 +6,9 @@ from tealight.art import screen_width, screen_height
 
 def linethree(x1,y1,z1,x2,y2,z2):
   a1=screen_width/2+x1*math.sqrt(3)/2-y1/2
-  b1=screen_height+x1/2+y1*math.sqrt(3)/2-z1
+  b1=screen_height/2+x1/2+y1*math.sqrt(3)/2-z1
   a2=screen_width+x2*math.sqrt(3)/2-y2/2
-  b2=screen_height+x2/2+y2*math.sqrt(3)/2-z2
+  b2=screen_height/2+x2/2+y2*math.sqrt(3)/2-z2
   line(a1,b1,a2,b2)
 
 linethree(0,0,0,100,0,0)
