0a2a6a9fea893ef868286ce48f3cc7639cae0134
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-19 12:21:14 +0100

    Running art/3d.py

--- art/3d.py
+++ art/3d.py
@@ -9,7 +9,7 @@ def linethree(x1,y1,z1,x2,y2,z2):
   b1=screen_height/2+x1/2+y1*math.sqrt(3)/2-z1
   a2=screen_width/2+x2*math.sqrt(3)/2-y2/2
   b2=screen_height/2+x2/2+y2*math.sqrt(3)/2-z2
-  line(a1,b1,a2,b2)
+    line(a1,b1,a2,b2)
   print a2-screen_width/2
   print b2-screen_height/2
 linethree(0,0,0,100,0,0)
