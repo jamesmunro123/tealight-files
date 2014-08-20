be7eaf589765b438b399ff408daa18e9c38b9f22
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 09:28:09 +0100

    Running art/squares.py

--- art/squares.py
+++ art/squares.py
@@ -3,6 +3,9 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 color("rgba(255,128,0,0.5)")
 
 
-def 
+def parallel(x1,y1,x2,y2,x3,y3):
+  for i in range(x1,x2)
+  line(i,(y2-y1)*(i-x1)/(x2-x1),x3-x1+i,(y2+y3-2*y1)*(i-x1)/(x2-x1)
+  
 
-box(100,100,100,200)
+parallel(10,20,30,40,50,60)
