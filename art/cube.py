d32ba8bf81a8afddc83a5e4b8fcfda60fc6201ba
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 11:50:22 +0100

    Running art/cube.py

--- art/cube.py
+++ art/cube.py
@@ -70,12 +70,12 @@ def handle_keyup(key):
 
   
 def parallel(x1,y1,x2,y2,x3,y3):
-  x1=int(x1)
-  y1=int(y1)
-  x2=int(x2)
-  y2=int(y2)
-  x3=int(x3)
-  y3=int(y3)
+  #x1=int(x1)
+  #y1=int(y1)
+  #x2=int(x2)
+  #y2=int(y2)
+  #x3=int(x3)
+  #y3=int(y3)
   if x2!=x1:
     for j in range(0,100):
       i=x1+(x2-x1)*j/100
