de33a90ee1aedbeeb8f537e18a87ebde89c26c21
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 12:28:18 +0100

    Running art/cube.py

--- art/cube.py
+++ art/cube.py
@@ -109,8 +109,10 @@ def handle_frame():
   color("white")
   box(0,0,screen_width,screen_height)
   
-  cube=[1,0,0,"black",-1,0,0,0,1,0,0,-1,0,0,0,1,0,0,-1]
-  print(cube[2:5])
+  cube=[1,0,0,"black",-1,0,0,"red",0,1,0,"green",0,-1,0,"yellow",0,0,1,"blue",0,0,-1,"white"]
+ 
+  for i in range(1,7)
+    
   
   color("black")
   side(-100,-100,-100,100,-100,-100,-100,-100,100)
