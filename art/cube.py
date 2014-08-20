892ff4b09dd70565065730c6bbaa3884f015af6d
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 10:12:17 +0100

    Running art/cube.py

--- art/cube.py
+++ art/cube.py
@@ -72,6 +72,12 @@ def handle_frame():
   alpha=alpha+dalpha
   
 def parallel(x1,y1,x2,y2,x3,y3):
+  x1=int(x1)
+  y1=int(y1)
+  x2=int(x2)
+  y2=int(y2)
+  x3=int(x3)
+  y3=int(y3)
  for i in range(x1,x2):
     line(i,(y2-y1)*(i-x1)/(x2-x1)+y1,x3-x1+i,(y2-y1)*(i-x1)/(x2-x1)+y3)
  
@@ -82,7 +88,6 @@ def side(x1,y1,z1,x2,y2,z2,x3,y3,z3):
   a3=xy3(x3,y3,z3)
   a5=a1+a2+a3
   print a5
-  a5=int(a5)
   parallel(*a5)
 
 color("white")
