325f4d6345c53a52b737a9172dc891025627ba9e
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 09:35:47 +0100

    Running art/squares.py

--- art/squares.py
+++ art/squares.py
@@ -8,6 +8,6 @@ def parallel(x1,y1,x2,y2,x3,y3):
     line(i,(y2-y1)*(i-x1)/(x2-x1)+y1,x3-x1+i,(y2-y1)*(i-x1)/(x2-x1)+y3)
   
 
-parallel(20,40,200,40,100,120)
+parallel(20,40,200,60,100,120)
 
 #line(i,(y2-y1)*(i-x1)/(x2-x1),x3-x1+i,(y2+y3-2*y1)*(i-x1)/(x2-x1))
