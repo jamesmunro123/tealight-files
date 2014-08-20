7bf36cb2f880941c2bde11fcaccf9a40093cf789
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 10:13:48 +0100

    Running art/squares.py

--- art/squares.py
+++ art/squares.py
@@ -4,6 +4,12 @@ color("rgba(255,128,0,1)")
 
 
 def parallel(x1,y1,x2,y2,x3,y3):
+  x1=int(x1)
+  y1=int(y1)
+  x2=int(x2)
+  y2=int(y2)
+  x3=int(x3)
+  y3=int(y3)
   x4=x2+x3-x1
   y4=y2+y3-y1
   for i in range(x1,x2):
