42a56970de591c3e3493cc12bcb942f93cd54e9d
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 12:10:07 +0100

    Running art/cube.py

--- art/cube.py
+++ art/cube.py
@@ -113,8 +113,9 @@ def handle_frame():
         ('john', 'A', 15),
         ('jane', 'B', 12),
         ('dave', 'B', 10),
-]
-  print(a)
+  ]
+  b=sorted(student_tuples, key=lambda student: student[2])
+  print(b)
   
   color("black")
   side(-100,-100,-100,100,-100,-100,-100,-100,100)
