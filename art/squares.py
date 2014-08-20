2e68b2516f55c27b336103d6a9133831fb52362d
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 09:28:35 +0100

    Running art/squares.py

--- art/squares.py
+++ art/squares.py
@@ -5,7 +5,7 @@ color("rgba(255,128,0,0.5)")
 
 def parallel(x1,y1,x2,y2,x3,y3):
   for i in range(x1,x2):
-  line(i,(y2-y1)*(i-x1)/(x2-x1),x3-x1+i,(y2+y3-2*y1)*(i-x1)/(x2-x1)
+  line(i,(y2-y1)*(i-x1)/(x2-x1),x3-x1+i,(y2+y3-2*y1)*(i-x1)/(x2-x1))
   
 
 parallel(10,20,30,40,50,60)
