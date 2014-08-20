ee739550852683067a1c94dad37bd2ad23c98534
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 09:19:24 +0100

    Running art/squares.py

--- art/squares.py
+++ art/squares.py
@@ -1,4 +1,4 @@
-from tealight.art import (color, line, spot, circle, box, image, text, background, color, rgba)
+from tealight.art import (color, line, spot, circle, box, image, text, background, color)
 
 color(rgba(255,128,0,0.5))
 
