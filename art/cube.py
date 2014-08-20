ae328be0ed84be2b6fb66b77925720312c67add8
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 09:53:02 +0100

    Running art/cube.py

--- art/cube.py
+++ art/cube.py
@@ -38,7 +38,7 @@ def xy3(x1,y1,z1,x2,y2,z2):
 #  b1=screen_height/2+x1/2+y1*math.sqrt(3)/2-z1
 #  a2=screen_width/2+x2*math.sqrt(3)/2-y2/2
 #  b2=screen_height/2+x2/2+y2*math.sqrt(3)/2-z2
-  return(e1,f1,e2,f2)
+  return (e1,f1,e2,f2)
   
   
 def handle_keydown(key):
@@ -84,7 +84,7 @@ def handle_frame():
   color("white")
   box(0,0,screen_width,screen_height)
   color("black")
-  xy3(-100,-100,-100,100,-100,-100)
+  (e1,f1,e2,f2) = xy3(-100,-100,-100,100,-100,-100)
   line(e1,f1,e2,f2)
   #linethree(100,-100,-100,100,-100,100)
   #linethree(100,-100,100,-100,-100,100)
