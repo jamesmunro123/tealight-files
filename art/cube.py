ffa1177e3fbd0562384d1bb37e247b86f2f6d272
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-20 10:10:57 +0100

    Running art/cube.py

--- art/cube.py
+++ art/cube.py
@@ -72,9 +72,7 @@ def handle_frame():
   alpha=alpha+dalpha
   
 def parallel(x1,y1,x2,y2,x3,y3):
-  x4=x2+x3-x1
-  y4=y2+y3-y1
-  for i in range(x1,x2):
+ for i in range(x1,x2):
     line(i,(y2-y1)*(i-x1)/(x2-x1)+y1,x3-x1+i,(y2-y1)*(i-x1)/(x2-x1)+y3)
  
     
