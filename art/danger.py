4dbc6403a44351c68616b27bd489205f9226fe0f
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 10:59:20 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -14,29 +14,29 @@ n=7
 for i in range (1,n+1):
   r1=randint(10,screen_width-10)
   r2=randint(10,screen_height-10)
-  r3=randint(0,400)/100
-  r4=randint(0,400)/100
+  r3=randint(0,400)/100-2
+  r4=randint(0,400)/100-2
   a=a+[r1,r2,r3,r4,0,0,255]
 
 for i in range (1,n+1):
   r1=randint(5,screen_width-5)
   r2=randint(5,screen_height-5)
-  r3=randint(0,400)/100
-  r4=randint(0,400)/100
+  r3=randint(0,400)/100-2
+  r4=randint(0,400)/100-2
   a=a+[r1,r2,r3,r4,255,0,255]
 
 for i in range (1,n+1):
   r1=randint(10,screen_width-10)
   r2=randint(10,screen_height-10)
-  r3=randint(0,400)/100
-  r4=randint(0,400)/100
+  r3=randint(0,400)/100-2
+  r4=randint(0,400)/100-2
   a=a+[r1,r2,r3,r4,255,0,0]
 
 for i in range (1,n+1):
   r1=randint(5,screen_width-5)
   r2=randint(5,screen_height-5)
-  r3=randint(0,400)/100
-  r4=randint(0,400)/100
+  r3=randint(0,400)/100-2
+  r4=randint(0,400)/100-2
   a=a+[r1,r2,r3,r4,255,100,0]
 
 def ctorgba(c):
