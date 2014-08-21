beaf4e99f6a744a7a394c3275b9076b7c38c29d6
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 15:18:19 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -168,7 +168,7 @@ def handle_frame():
       a[d*i+2]=((n2*n2-e2*n1*n1)*v1x-(1+e2)*n1*n2*v1y)/nn
       a[d*i+3]=(-(1+e2)*n1*n2*v1x+(n1*n1-e2*n2*n2)*v1y)/nn
     if (a[d*i]-screen_width/2+domain/2)*(a[d*i]-screen_width/2+domain/2)+(a[d*i+1]-screen_height/2)*(a[d*i+1]-screen_height/2)>hipporadius*hipporadius and hippo1==1:
-      print("Nom")
+      print("Nom",i)
   
   color("rgba(255,0,0,1)")
   circle(screen_width/2-domain/2,screen_height/2,hipporadius)
