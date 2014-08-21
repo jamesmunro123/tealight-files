f70d12985350755c31594cd523a3e5d9bd8df288
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 14:15:01 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -99,7 +99,7 @@ def handle_frame():
             a[d*j+2]=((n2*n2-n1*n1)*v2x-2*n1*n2*v2y)/nn
             a[d*j+3]=(-2*n1*n2*v2x+(n1*n1-n2*n2)*v2y)/nn
             a[d*i+7]=radius/(abs(a[d*i+2])+0.1)
-            a[d*j+7]=10
+            a[d*j+7]=radius/(abs(a[d*j+2])+0.1)
     if a[d*i+7]!=0:
       a[d*i+7]=a[d*i+7]-1
             #if a[7*i]*a[7*i]+a[7*i+1]*a[7*i+1]>10000:
