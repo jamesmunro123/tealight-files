f3857aa17ba6f0183dc35b5f3faa21b88fab7b7a
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 10:51:52 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -10,8 +10,8 @@ a=[]
 n=7
 
 for i in range (1,n+1):
-  r1=randint(5,screen_width-5)
-  r2=randint(5,screen_height-5)
+  r1=randint(10,screen_width-10)
+  r2=randint(10,screen_height-10)
   a=a+[r1,r2,1,0,0,0,255]
 
 for i in range (1,n+1):
@@ -19,12 +19,22 @@ for i in range (1,n+1):
   r2=randint(5,screen_height-5)
   a=a+[r1,r2,1,0,255,0,255]
 
+for i in range (1,n+1):
+  r1=randint(10,screen_width-10)
+  r2=randint(10,screen_height-10)
+  a=a+[r1,r2,1,0,255,0,0]
+
+for i in range (1,n+1):
+  r1=randint(5,screen_width-5)
+  r2=randint(5,screen_height-5)
+  a=a+[r1,r2,1,0,255,100,0]
+
 def ctorgba(c):
   string_out = "rgba("
   string_out = string_out + str(c[0]) + "," + str(c[1]) + "," + str(c[2]) + "," + str(c[3]) + ")"
   return string_out
 
-for i in range (0,2*n):
+for i in range (0,4*n):
   c=[a[7*i+4],a[7*i+5],a[7*i+6],1]
   colstring = ctorgba(c)
   color(colstring)
