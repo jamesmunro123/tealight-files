c936461c50de1d0bd5b3cde1a38bfe21303a4edb
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 12:21:04 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -19,9 +19,10 @@ for i in range (0,4*n):
   r3=randint(0,s1*100)/100-s1/2
   r4=randint(0,s1*100)/100-s1/2
   a=a+[r1,r2,r3,r4,0,0,255]
+  print c
   while c==1:
     c=0
-    print i
+    #print i
     for j in range (0,i):
       n1=(a[7*i]-a[7*j])
       n2=(a[7*i+1]-a[7*j+1])
