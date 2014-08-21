5ce8cd6638399a310ffe5f8c3caed2f177eda257
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 10:32:21 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -9,8 +9,8 @@ from random import (random, randint)
 a=[]
 
 for i in range (1,3):
-  r1=randint(0,screen_width)
-  r2=randint(0,screen_height)
+  r1=randint(0,screen_width-5)
+  r2=randint(0,screen_height-5)
   a=a+[r1,r2,1,0,0,0,0]
   
 
