ae920e9112ee373605f2d612301dcff03f056ad4
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 10:25:38 +0100

    Running art/danger.py

---
+++ art/danger.py
@@ -0,0 +1,18 @@
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+import math
+
+from tealight.art import screen_width, screen_height
+
+from random import random
+
+a=()
+
+for i in range (1,3)
+  r=randint(0,100)
+  a=a+(r,r,1,0,0,0,0)
+
+  
+for i in range (1,3)
+  spot(r,r,10)
+  
