e14e5f313791047904ece9bbd79fc43931310616
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 14:45:12 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -78,7 +78,9 @@ def handle_frame():
   for i in range (0,4*n):
     a[d*i]=a[d*i]+a[d*i+2]
     a[d*i+1]=a[d*i+1]+a[d*i+3]
-  
+    a[d*i+2]=a[d*i+2]*0.9
+    a[d*i+3]=a[d*i+3]*0.9
+    
   for i in range (0,4*n):
     c=[a[d*i+4],a[d*i+5],a[d*i+6],1]
     colstring = ctorgba(c)
