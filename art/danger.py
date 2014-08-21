99b174dbcb321492ca0a599cd07da8a7f6f7b9f1
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 10:32:44 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -7,14 +7,15 @@ from tealight.art import screen_width, screen_height
 from random import (random, randint)
 
 a=[]
+n=100
 
-for i in range (1,3):
+for i in range (1,n):
   r1=randint(5,screen_width-5)
   r2=randint(5,screen_height-5)
   a=a+[r1,r2,1,0,0,0,0]
   
 
 print(a) 
-for i in range (0,2):
+for i in range (0,n-1):
   spot(a[7*i],a[7*i+1],10)
   
