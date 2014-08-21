30309df91eaf111153ccd478a1e51f79637e039b
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 14:52:21 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -1,6 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-import math
+from math import (sin, cos, pi)
 
 from tealight.art import screen_width, screen_height
 
