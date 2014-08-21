9a76c23bd94059eed99bc345e09bc22a04639748
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-21 11:06:58 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -65,7 +65,7 @@ def handle_frame():
     
   for i in range (0,4*n):
     for j in range (i,4*n):
-      if (a[7*i]-a[7*j])^2+(a[7*i+1]-a[7*j+1])^2<10:
+      if (a[7*i]-a[7*j])^2+(a[7*i+1]-a[7*j+1])^2<1:
         print("crash!")
         a[7*i+2]=0
         a[7*i+3]=0
