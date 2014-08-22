4c48f6d62df9efd7d970afcb1107eadd7fea4e47
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 09:38:39 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -319,8 +319,8 @@ def handle_frame():
       spot(screen_width/2-domain/2+hipporadius/3,screen_height/2+hipporadius/3,hipporadius/5)
       spot(screen_width/2-domain/2+hipporadius/3,screen_height/2-hipporadius/3,hipporadius/5)
       color("black")
-      spot(screen_width/2-domain/2+hipporadius/3,screen_height/2+hipporadius/3,hipporadius/10)
-      spot(screen_width/2-domain/2+hipporadius/3,screen_height/2-hipporadius/3,hipporadius/10)
+      spot(screen_width/2-domain/2+1.1*hipporadius/3,screen_height/2+hipporadius/3,hipporadius/10)
+      spot(screen_width/2-domain/2+1.1*hipporadius/3,screen_height/2-hipporadius/3,hipporadius/10)
     if hippo2==1:
       color("rgba(255,0,255,1)")
       spot(screen_width/2,screen_height/2-domain/2,hipporadius)
