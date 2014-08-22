c19a720b9c50a48606dcb26ec42d678293b76dc3
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 19:22:00 +0100

    Running art/balls.py

--- art/balls.py
+++ art/balls.py
@@ -11,7 +11,7 @@ from random import (random, randint)
 from tealight.utils import sleep
 
 a=[]
-n=100
+n=50
 s1=15
 e=0.1
 e2=0.1
@@ -22,7 +22,7 @@ g=0.7
 redcount=2*n
 bluecount=2*n
 domain=min(screen_width,screen_height)/2-10
-radius=5
+radius=10
 domain=domain-radius
 hipporadius=domain/4
 #(x,y,u,v,colr,colg,colb,inert,ax,ay)
