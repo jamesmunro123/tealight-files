040fdb130bd49578760092ef11ba98b4097690ba
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 19:13:31 +0100

    Running art/balls.py

--- art/balls.py
+++ art/balls.py
@@ -62,26 +62,6 @@ for i in range (0,n):
         a[d*i]=r1
         a[d*i+1]=r2
 
-#for i in range (1,n+1):
-#  r1=randint(5,screen_width-5)
-#  r2=randint(5,screen_height-5)
-#  r3=randint(0,s1*100)/100-s1/2
-#  r4=randint(0,s1*100)/100-s1/2
-#  a=a+[r1,r2,r3,r4,255,0,255]
-
-#for i in range (1,n+1):
-#  r1=randint(10,screen_width-10)
-#  r2=randint(10,screen_height-10)
-#  r3=randint(0,s1*100)/100-s1/2
-#  r4=randint(0,s1*100)/100-s1/2
-#  a=a+[r1,r2,r3,r4,255,0,0]
-
-#for i in range (1,n+1):
-#  r1=randint(5,screen_width-5)
-#  r2=randint(5,screen_height-5)
-#  r3=randint(0,s1*100)/100-s1/2
-#  r4=randint(0,s1*100)/100-s1/2
-#  a=a+[r1,r2,r3,r4,255,100,0]
 
 def ctorgba(c):
   string_out = "rgba("
