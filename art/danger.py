0fd927a2a3e069205ce7ec16f806208fe8190d34
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 10:06:15 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -367,8 +367,9 @@ def handle_frame():
       spot(screen_width/2+domain/2-1.2*hipporadius/3,screen_height/2+hipporadius/3,hipporadius/10)
       spot(screen_width/2+domain/2-1.2*hipporadius/3,screen_height/2-hipporadius/3,hipporadius/10)
  
-    color("black")  
+    color("red")  
     text(10,10,sr)
+    color("blue")
     text(screen_width-100,10,sb)
     
     if redcount==0 or bluecount==0:
