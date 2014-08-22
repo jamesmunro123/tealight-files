8e3fe72a7b27c8153077bed3680ce1b53b68dae1
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 09:28:18 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -187,8 +187,10 @@ def handle_frame():
         n2=hcy
         v1x=a[d*i+2]
         v1y=a[d*i+3]
-        a[d*i+2]=((n2*n2-e*n1*n1)*v1x-(1+e)*n1*n2*v1y)/nn
-        a[d*i+3]=(-(1+e)*n1*n2*v1x+(n1*n1-e*n2*n2)*v1y)/nn
+        a[d*i+2]=((n2*n2-e*n1*n1)*v1x-(1+e3)*n1*n2*v1y)/nn
+        a[d*i+3]=(-(1+e3)*n1*n2*v1x+(n1*n1-e3*n2*n2)*v1y)/nn
+        #a[d*i]=a[d*i]+(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
+        #a[d*i+1]=a[d*i+1]+(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
       if (a[d*i]-screen_width/2+domain/2)*(a[d*i]-screen_width/2+domain/2)+(a[d*i+1]-screen_height/2)*(a[d*i+1]-screen_height/2)<hipporadius*hipporadius and hippo1==1:
         a[d*i]=1000000
         a[d*i+1]=1000000
