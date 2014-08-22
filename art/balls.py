3fbed4cfcc2a28287d3ec77505449995dd1da23f
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 19:42:33 +0100

    Running art/balls.py

--- art/balls.py
+++ art/balls.py
@@ -44,9 +44,9 @@ for i in range (0,n):
   r2=r5*sin(r6)+screen_height/2
   r3=randint(0,s1*100)/100-s1/2
   r4=randint(0,s1*100)/100-s1/2
-  r7=randint(256)
-  r8=randint(256)
-  r9=randint(256)
+  r7=randint(0,256)
+  r8=randint(0,256)
+  r9=randint(0,256)
   a=a+[r1,r2,r3,r4,r7,r8,r9,0,0,g]
  
   
