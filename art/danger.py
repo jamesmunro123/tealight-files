d8cb59e4a83193904c025f7369b1bb1ea8d0f82c
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-22 09:02:51 +0100

    Running art/danger.py

--- art/danger.py
+++ art/danger.py
@@ -141,7 +141,7 @@ def handle_frame():
     spot(a[d*i],a[d*i+1],radius)
     
   for i in range (0,4*n+1):
-    for j in range (i+1,4*n):
+    for j in range (i+1,4*n+1):
       n1=(a[d*i]-a[d*j])
       n2=(a[d*i+1]-a[d*j+1])
       nn=n1*n1+n2*n2
