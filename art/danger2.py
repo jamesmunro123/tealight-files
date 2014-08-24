9ad2dad7df8bccf20e620172faf027d55ac3b7e5
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-24 21:52:54 +0100

    Running art/danger2.py

--- art/danger2.py
+++ art/danger2.py
@@ -22,7 +22,7 @@ mu=0
 redcount=2*n
 bluecount=2*n
 domain=min(screen_width,screen_height)/2-10
-radius=domain/6
+radius=domain/10
 domain=domain-radius
 hipporadius=domain/4
 #(x,y,u,v,colr,colg,colb,inert,ax,ay)
