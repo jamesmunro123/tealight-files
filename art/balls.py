2c082a68e247468b704495cfeacf2ce898bfcc27
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-24 17:10:51 +0100

    Running art/balls.py

--- art/balls.py
+++ art/balls.py
@@ -106,17 +106,32 @@ def handle_frame():
       n1=(a[d*i]-a[d*j])
       n2=(a[d*i+1]-a[d*j+1])
       nn=n1*n1+n2*n2
-      v1x=a[d*i+2]
-      v1y=a[d*i+3]
-      v2x=a[d*j+2]
-      v2y=a[d*j+3]
       if nn<4*radius*radius:
         if nn !=0:
           if a[d*i+7]==0 and a[d*i+7]==0:
-            a[d*i+2]=((n2*n2-e*n1*n1)*v1x-(1+e)*n1*n2*v1y)/nn
-            a[d*i+3]=(-(1+e)*n1*n2*v1x+(n1*n1-e*n2*n2)*v1y)/nn
-            a[d*j+2]=((n2*n2-n1*n1)*v2x-2*n1*n2*v2y)/nn
-            a[d*j+3]=(-2*n1*n2*v2x+(n1*n1-n2*n2)*v2y)/nn
+            v1x=a[d*i+2]
+            v1y=a[d*i+3]
+            v2x=a[d*j+2]
+            v2y=a[d*j+3]
+      
+            v3x=n1*v1x+n2*v1y
+            v3y=-n2*v1x+n1*v1y
+            v4x=n1*v2x+n2*v2y
+            v4y=-n2*v2x+n1*v2y
+            
+            v5x=0.5*((1-e)*v3x+(1+e)*v4x)
+            v6x=0.5*((1-e)*v4x+(1+e)*v3x)
+            
+            v7x=(n1*v5x-n2*v3y)/nn
+            v7y=(n2*v5x+n1*v3y)/nn
+            v8x=(n1*v6x-n2*v4y)/nn
+            v8y=(n2*v6x+n1*v4y)/nn
+            a[d*i+2]=v7x
+            a[d*i+3]=v7y
+            a[d*j+2]=v8x
+            a[d*j+3]=v8y
+              
+            
             a[d*i]=a[d*i]+(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
             a[d*i+1]=a[d*i+1]+(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
             a[d*j]=a[d*j]-(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
