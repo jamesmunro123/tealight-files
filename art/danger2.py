01efdd0cc1b373c708fbba080a392d9abdb6ce4c
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-24 16:41:49 +0100

    Running art/danger2.py

--- art/danger2.py
+++ art/danger2.py
@@ -199,10 +199,10 @@ def handle_frame():
         if nn<4*radius*radius:
           if nn !=0:
             if a[d*i+7]==0 and a[d*i+7]==0:
-              a[d*i+2]=v7x
-              a[d*i+3]=v7y
-              a[d*j+2]=v8x
-              a[d*j+3]=v8y
+              a[d*i+2]=v8x
+              a[d*i+3]=v8y
+              a[d*j+2]=v7x
+              a[d*j+3]=v7y
               
               a[d*i]=a[d*i]+(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
               a[d*i+1]=a[d*i+1]+(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
