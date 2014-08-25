be0eedbd45e9002a62122e43f4ad850a68391958
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-25 18:16:57 +0100

    Running art/pool.py

--- art/pool.py
+++ art/pool.py
@@ -60,7 +60,7 @@ for i in range (0,n):
       nn=n1*n1+n2*n2
       
       if nn<4*radius*radius:
-        c=1
+        c=0
         r5=randint(0,domain)
         r6=randint(0,100)*(360)/100
         r1=r5*cos(r6)+screen_width/2
