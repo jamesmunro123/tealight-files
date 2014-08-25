d8964e5a9f9c05b8c9d8133aa848f725a73bf7ec
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-25 12:35:16 +0100

    Running art/pool.py

--- art/pool.py
+++ art/pool.py
@@ -49,11 +49,9 @@ for i in range (0,n):
   r9=randint(0,256)
   a=a+[r1,r2,0,0,r7,r8,r9,0,0,g]
  
-a[d*(n-1)+4]=0
-a[d*(n-1)+5]=0
-a[d*(n-1)+6]=0
 
- while c==1:
+
+  while c==1:
     c=0
     for j in range (0,i):
       n1=(a[d*i]-a[d*j])
