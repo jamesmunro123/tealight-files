578ad4af6ac88a2923a6cee50d3380f02cb72678
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-25 12:37:47 +0100

    Running art/pool.py

--- art/pool.py
+++ art/pool.py
@@ -83,6 +83,11 @@ def handle_mousedown(x,y):
   a[d*(n-1)+2]=s1*a1/sqrt(aa)
   a[d*(n-1)+3]=s1*a2/sqrt(aa)
 
+def handle_mousemove(x,y):
+  global n, a
+  color("rbga(0,0,0,0)")
+  line(a[d*(n-1)],a[d*(n-1)+1],x,y)
+  
 def handle_frame():
     
   sleep(20)
