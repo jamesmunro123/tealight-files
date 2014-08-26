1864e8be52b356b97893128941b1d4fc5c296563
jamesmunro123 <jpm82@cam.ac.uk>
2014-08-26 18:03:39 +0100

    Running art/spacepool.py

--- art/spacepool.py
+++ art/spacepool.py
@@ -27,7 +27,7 @@ domain=domain-radius
 hipporadius=domain/4
 cx=screen_width/2
 cy=screen_height/2
-#(x,y,u,v,colr,colg,colb)
+#(x,y,u,v,colr,colg,colb,out)
 d=7
 
 
@@ -47,9 +47,9 @@ for i in range (0,n):
   #r4=randint(0,s1*100)/100-s1/2
   
   if i<int(n/2):
-    a=a+[r1,r2,0,0,255,0,0]
+    a=a+[r1,r2,0,0,255,0,0,0]
   else:
-    a=a+[r1,r2,0,0,0,0,255]
+    a=a+[r1,r2,0,0,0,0,255,0]
   while c==1:
     c=0
     for j in range (0,i):
@@ -97,63 +97,61 @@ def handle_frame():
   circle(cx,cy,2*radius)
   
   for i in range (0,n):
-
-    a[d*i]=a[d*i]+a[d*i+2]
-    a[d*i+1]=a[d*i+1]+a[d*i+3]
-    
-    a[d*i]=a[d*i]%screen_width
-    a[d*i+1]=a[d*i+1]%screen_height
-    
-    #Collisions   
-    for j in range (i+1,n):
-      n1=(a[d*i]-a[d*j])
-      n2=(a[d*i+1]-a[d*j+1])
-      nn=n1*n1+n2*n2
-      if nn<4*radius*radius:
-        if nn !=0:
-          v1x=a[d*i+2]
-          v1y=a[d*i+3]
-          v2x=a[d*j+2]
-          v2y=a[d*j+3]
-    
-          v3x=n1*v1x+n2*v1y
-          v3y=-n2*v1x+n1*v1y
-          v4x=n1*v2x+n2*v2y
-          v4y=-n2*v2x+n1*v2y
-          
-          v5x=0.5*((1-e)*v3x+(1+e)*v4x)
-          v6x=0.5*((1-e)*v4x+(1+e)*v3x)
-          
-          v7x=(n1*v5x-n2*v3y)/nn
-          v7y=(n2*v5x+n1*v3y)/nn
-          v8x=(n1*v6x-n2*v4y)/nn
-          v8y=(n2*v6x+n1*v4y)/nn
-          a[d*i+2]=v7x
-          a[d*i+3]=v7y
-          a[d*j+2]=v8x
-          a[d*j+3]=v8y
-            
-          
-          a[d*i]=a[d*i]+(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
-          a[d*i+1]=a[d*i+1]+(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
-          a[d*j]=a[d*j]-(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
-          a[d*j+1]=a[d*j+1]-(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
-    
-    #Pocket
-    if (a[d*i]-cx)**2+(a[d*i+1]-cy)**2<4*radius**2 and i!=n-1:
-      print("Ding!")
-      a[d*i]=None
-      a[d*i+1]=None
-      a[d*i+2]=0
-      a[d*i+3]=0
+    if a[d*i+7]!=1:
+      a[d*i]=a[d*i]+a[d*i+2]
+      a[d*i+1]=a[d*i+1]+a[d*i+3]
       
-            
-    v=min(100*int(sqrt(a[d*i+2]**2+a[d*i+3]**2)),255)
-    #c=[v,256-v,0,1]
-    c=[a[d*i+4],a[d*i+5],a[d*i+6],1]
-    colstring = ctorgba(c)
-    color(colstring)
-    spot(a[d*i],a[d*i+1],radius)
+      a[d*i]=a[d*i]%screen_width
+      a[d*i+1]=a[d*i+1]%screen_height
+      
+      #Collisions   
+      for j in range (i+1,n):
+        if a[d*j+7]!=1
+          n1=(a[d*i]-a[d*j])
+          n2=(a[d*i+1]-a[d*j+1])
+          nn=n1*n1+n2*n2
+          if nn<4*radius*radius:
+            if nn !=0:
+              v1x=a[d*i+2]
+              v1y=a[d*i+3]
+              v2x=a[d*j+2]
+              v2y=a[d*j+3]
+        
+              v3x=n1*v1x+n2*v1y
+              v3y=-n2*v1x+n1*v1y
+              v4x=n1*v2x+n2*v2y
+              v4y=-n2*v2x+n1*v2y
+              
+              v5x=0.5*((1-e)*v3x+(1+e)*v4x)
+              v6x=0.5*((1-e)*v4x+(1+e)*v3x)
+              
+              v7x=(n1*v5x-n2*v3y)/nn
+              v7y=(n2*v5x+n1*v3y)/nn
+              v8x=(n1*v6x-n2*v4y)/nn
+              v8y=(n2*v6x+n1*v4y)/nn
+              a[d*i+2]=v7x
+              a[d*i+3]=v7y
+              a[d*j+2]=v8x
+              a[d*j+3]=v8y
+                
+              
+              a[d*i]=a[d*i]+(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
+              a[d*i+1]=a[d*i+1]+(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
+              a[d*j]=a[d*j]-(0+radius-sqrt(nn)/2)*n1/sqrt(nn)
+              a[d*j+1]=a[d*j+1]-(0+radius-sqrt(nn)/2)*n2/sqrt(nn)
+        
+      #Pocket
+      if (a[d*i]-cx)**2+(a[d*i+1]-cy)**2<4*radius**2 and i!=n-1:
+        print("Ding!")
+        a[d*i+7]=1
+        
+              
+      v=min(100*int(sqrt(a[d*i+2]**2+a[d*i+3]**2)),255)
+      #c=[v,256-v,0,1]
+      c=[a[d*i+4],a[d*i+5],a[d*i+6],1]
+      colstring = ctorgba(c)
+      color(colstring)
+      spot(a[d*i],a[d*i+1],radius)
   color("rbga(0,0,0,0)")
   line(a[d*(n-1)],a[d*(n-1)+1],mx,my)
   
